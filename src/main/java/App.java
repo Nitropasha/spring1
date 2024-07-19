11c8e3a06e4bd550fa8a5c7abce0bfc75820e562
nitropasha <djioeff@gmail.com>
2024-07-19 15:28:22 +0500

    Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
